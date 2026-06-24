3c22b4eb01451fa53454bffaeeda7e80d6b13557
Isabel de Wijn <idewij27@igs.vic.edu.au>
2026-06-24 10:06:06 +1000

    Basic Setup
    Css injection now working. Background colour set up, and two main fonts imported.

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown(
-    "<h2 style='text-align: left;'>Lexicon</h2>",
-    unsafe_allow_html=True,
+# Function: Load CSS - Takes CSS file_path parameter
+#Keeping the CSS in ints own file helps the main python code to be more readable and removes the need for <style> tags at every element
+def load_css(file_path):
+    with open(file_path) as f:
+        st.html(f"<style>{f.read()}</style>")
+
+#Load CSS
+load_css("style.css")
+
+#Page elements:
+st.markdown("""
+    <div class='logo'>Lexicon</div>
+    """, unsafe_allow_html=True)
+st.divider()
+st.markdown("""
+    <div class='sub-heading'>Currently Reading</div>
+    """,unsafe_allow_html=True
 )
-st.button ("Test")
+st.divider()
+
+
+
+
